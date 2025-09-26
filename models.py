f96a102032c067821eb82d4d6e0cf87de13d753c
hide1210fn-boop <hide1210fn@gmail.com>
2025-09-26 09:42:00 +0900

    投稿日時追加

--- models.py
+++ models.py
@@ -4,6 +4,8 @@
 # ----------------------------------------
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 # DBインスタンス（Flask本体から分離）
 db = SQLAlchemy()
@@ -16,6 +18,7 @@ class Review(db.Model):
     spot_name = db.Column(db.String(100), nullable=False)  # 観光地名
     comment = db.Column(db.Text, nullable=False)  # コメント
     rating = db.Column(db.Integer, nullable=False)  # 評価（1〜5）
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<Review {self.spot_name} - {self.rating}★>'
