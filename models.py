b28c95f00a2d00c88fd3dfed32759497ca04ab69
hide1210fn-boop <hide1210fn@gmail.com>
2025-09-26 09:48:49 +0900

    コメント追加

--- models.py
+++ models.py
@@ -18,7 +18,8 @@ class Review(db.Model):
     spot_name = db.Column(db.String(100), nullable=False)  # 観光地名
     comment = db.Column(db.Text, nullable=False)  # コメント
     rating = db.Column(db.Integer, nullable=False)  # 評価（1〜5）
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # レビュー投稿日時（自動で現在時刻を記録）
+
 
     def __repr__(self):
         return f'<Review {self.spot_name} - {self.rating}★>'
